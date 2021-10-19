1033d6204a5c4edad8a78fd52183e3c36a55e2d8
ArnaldoObispo <92825230+ArnaldoObispo@users.noreply.github.com>
2021-10-19 16:31:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Programacion_I
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 4;
+            int b = 2;
+            int c = 0;
+
+            
+            Console.WriteLine("aacb=");
+
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+
+            Console.WriteLine("bca=");
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+
+            Console.WriteLine("bcbc=");
+            
+
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+
+    
+
+
+
+        }
+    }
+}
